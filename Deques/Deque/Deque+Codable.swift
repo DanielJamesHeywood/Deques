862d387c3262f6293b88ad2a5ec40a0bf9c4178a
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-09-23 18:42:18 +0100

    Declared Decodable conformance.

--- Deques/Deque/Deque+Codable.swift
+++ Deques/Deque/Deque+Codable.swift
@@ -9,3 +9,15 @@ extension Deque: Encodable where Element: Encodable {
         }
     }
 }
+
+extension Deque: Decodable where Element: Decodable {
+    
+    @inlinable
+    public init(from decoder: any Decoder) throws {
+        self.init()
+        var container = try decoder.unkeyedContainer()
+        while !container.isAtEnd {
+            self.append(try container.decode(Element.self))
+        }
+    }
+}
