c240b8d1539baef930bdc9e25673b810153c1c8e
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-09-23 17:25:46 +0100

    Declared Collection conformance.

---
+++ Deques/Deque/Deque+Collection.swift
@@ -0,0 +1,39 @@
+
+extension Deque: Collection {
+    
+    @inlinable
+    public var startIndex: Index {
+        return Index(_startOf: self)
+    }
+    
+    @inlinable
+    public var endIndex: Index {
+        return Index(_endOf: self)
+    }
+    
+    @inlinable
+    public subscript(position: Index) -> Element {
+        get {
+            guard let node = position._handle?.takeUnretainedValue() else {
+                preconditionFailure()
+            }
+            return node.element
+        }
+    }
+    
+    @inlinable
+    public func index(after index: Index) -> Index {
+        var newIndex = index
+        formIndex(after: &newIndex)
+        return newIndex
+    }
+    
+    @inlinable
+    public func formIndex(after index: inout Index) {
+        guard let node = index._handle?.takeUnretainedValue() else {
+            preconditionFailure()
+        }
+        index._handle = node.next.map(Unmanaged.passUnretained)
+        index._offset += 1
+    }
+}
