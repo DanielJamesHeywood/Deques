e8749beb6946125eaf26e786959baea7c90c6984
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-09-23 12:50:49 +0100

    Stylistic changes to ==.

--- Deques/Deque/Deque.Index/Deque.Index+Equatable.swift
+++ Deques/Deque/Deque.Index/Deque.Index+Equatable.swift
@@ -2,7 +2,7 @@
 extension Deque.Index: Equatable {
     
     @inlinable
-    public static func == (lhs: Deque<Element>.Index, rhs: Deque<Element>.Index) -> Bool {
+    public static func == (lhs: Deque.Index, rhs: Deque.Index) -> Bool {
         lhs._ensureValid(with: rhs)
         return lhs._offset == rhs._offset
     }
