cc06e4bc4667fbc9a81bfb15869412edec16a704
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-09-23 17:08:00 +0100

    Declared Comparable conformance for Index.

---
+++ Deques/Deque/Deque.Index/Deque.Index+Comparable.swift
@@ -0,0 +1,9 @@
+
+extension Deque.Index: Comparable {
+    
+    @inlinable
+    public static func < (lhs: Deque.Index, rhs: Deque.Index) -> Bool {
+        lhs._ensureValid(with: rhs)
+        return lhs._offset < rhs._offset
+    }
+}
