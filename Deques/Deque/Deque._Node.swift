081207daab024a096b04f80d8c5039545da05faa
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-09-23 17:37:04 +0100

    Modified _Node for bidirectional iteration.

--- Deques/Deque/Deque._Node.swift
+++ Deques/Deque/Deque._Node.swift
@@ -11,9 +11,13 @@ extension Deque {
         internal let next: _Node?
         
         @usableFromInline
-        internal init(element: Element, next: _Node? = nil) {
+        internal let previous: Unmanaged<_Node>?
+        
+        @usableFromInline
+        internal init(element: Element, next: _Node? = nil, previous: _Node? = nil) {
             self.element = element
             self.next = next
+            self.previous = previous.map(Unmanaged.passUnretained)
         }
     }
 }
