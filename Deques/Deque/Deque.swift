379f5183db3a482a037123ee0a586972d4dfbfe9
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-09-23 18:12:11 +0100

    Declared empty initialiser.

--- Deques/Deque/Deque.swift
+++ Deques/Deque/Deque.swift
@@ -13,4 +13,12 @@ public struct Deque<Element> {
     
     @usableFromInline
     internal var _version: _Version
+    
+    @inlinable
+    public init() {
+        self._head = nil
+        self._tail = nil
+        self._count = 0
+        self._version = _Version()
+    }
 }
