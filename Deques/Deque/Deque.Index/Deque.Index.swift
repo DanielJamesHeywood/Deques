79000cc6af9127aaa5c15eda1b2b0801af39e974
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-09-23 11:11:32 +0100

    Completed Index declaration.

--- Deques/Deque/Deque.Index/Deque.Index.swift
+++ Deques/Deque/Deque.Index/Deque.Index.swift
@@ -2,5 +2,29 @@
 extension Deque {
     
     @frozen
-    public struct Index {}
+    public struct Index {
+        
+        @usableFromInline
+        internal var _handle: Unmanaged<_Node>?
+        
+        @usableFromInline
+        internal var _offset: Int
+        
+        @usableFromInline
+        internal let _version: _Version
+        
+        @inlinable
+        internal init(_startOf base: Deque) {
+            self._handle = base._head.map(Unmanaged.passUnretained)
+            self._offset = 0
+            self._version = base._version
+        }
+        
+        @inlinable
+        internal init(_endOf base: Deque) {
+            self._handle = nil
+            self._offset = base._count
+            self._version = base._version
+        }
+    }
 }
