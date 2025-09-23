fe0047f689a7999763c9502d77fd105933598062
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-09-23 17:09:11 +0100

    Extended Comparable declaration to include all required methods.

--- Deques/Deque/Deque.Index/Deque.Index+Comparable.swift
+++ Deques/Deque/Deque.Index/Deque.Index+Comparable.swift
@@ -6,4 +6,22 @@ extension Deque.Index: Comparable {
         lhs._ensureValid(with: rhs)
         return lhs._offset < rhs._offset
     }
+    
+    @inlinable
+    public static func <= (lhs: Deque.Index, rhs: Deque.Index) -> Bool {
+        lhs._ensureValid(with: rhs)
+        return lhs._offset <= rhs._offset
+    }
+    
+    @inlinable
+    public static func >= (lhs: Deque.Index, rhs: Deque.Index) -> Bool {
+        lhs._ensureValid(with: rhs)
+        return lhs._offset >= rhs._offset
+    }
+    
+    @inlinable
+    public static func > (lhs: Deque.Index, rhs: Deque.Index) -> Bool {
+        lhs._ensureValid(with: rhs)
+        return lhs._offset > rhs._offset
+    }
 }
